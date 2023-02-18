dc66a9b30839cbfbb70a261fb65067cbb4b1a081
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 09:37:32 -0300

    create repository private

--- vars/createRepositorySpringGitlab.groovy
+++ vars/createRepositorySpringGitlab.groovy
@@ -2,12 +2,10 @@ def call() {
     node {
 
         stage("Checkout") {
-            log("debug", "Entrou no checkout")
             gitCheckoutGitlab()
         }
 
         stage("Create Project") {
-            log("debug", "Entrou no Create")
             createRepositoryGitlab()
         }
 
--- vars/gitCheckoutGitlab.groovy
+++ vars/gitCheckoutGitlab.groovy
@@ -2,11 +2,13 @@ import br.dev.pedrodavi.Constants
 
 def call(){
     log("info", "Clonando repositório do Archetype")
-    git branch: "main", credentialsId: Constants.JENKINS_GITLAB_CREDENTIALS_ID, url: "https://gitlab.com/srvexbr/archetypes/spring-boot/backend/api-rest-spring-boot-archetype.git"
-    sh "rm -r .git"
-    sh "mvn clean install"
-    sh "ls -la"
-    sh "pwd"
-    cleanWs()
+    withCredentials([gitUsernamePassword(credentialsId: Constants.JENKINS_GITLAB_CREDENTIALS_ID,  variable: 'JENKINSGITLABTOKEN', gitToolName: 'Default')]) {
+        git branch: "main", credentialsId: ${JENKINSGITLABTOKEN}, url: "https://gitlab.com/srvexbr/archetypes/spring-boot/backend/api-rest-spring-boot-archetype.git"
+        sh "rm -r .git"
+        sh "mvn clean install"
+        sh "ls -la"
+        sh "pwd"
+        cleanWs()
+    }
 }
 
