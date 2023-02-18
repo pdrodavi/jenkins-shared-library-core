a9b6295f345ac9d619661163d388a9562b53cc53
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 04:45:50 -0300

    create pipe

--- src/br/dev/pedrodavi/Constants.groovy
+++ src/br/dev/pedrodavi/Constants.groovy
@@ -3,5 +3,6 @@ package br.dev.pedrodavi
 class Constants {
     public static final String JENKINS_GITHUB_CREDENTIALS_ID = "github-token"
     public static final String JENKINS_JFROG_CREDENTIALS_ID = "JFROG-PLATAFORM"
+    public static final String JENKINS_CLI_CREDENTIALS_ID = "cli-jenkins-token"
     public static final String JENKINS_JFROG_URL_REGISTRY = "https://srvextechnology.jfrog.io/artifactory/registry-docker/"
 }
