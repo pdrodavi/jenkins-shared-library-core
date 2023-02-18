aa2a3d3103d15e89e1cf4824fef027cd8111a32d
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 04:30:19 -0300

    create pipe

--- vars/downloadCli.groovy
+++ vars/downloadCli.groovy
@@ -4,6 +4,7 @@ def call() {
     sh "curl -# -O http://host.docker.internal:8080/jnlpJars/jenkins-cli.jar"
     sh "pwd"
     sh "ls -la"
+    sh "java -jar jenkins-cli.jar -s http://host.docker.internal:8080/ -webSocket help"
 
 }
 
