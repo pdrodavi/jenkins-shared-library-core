f74f89316a5ed37ecd402d87d10a178c56bd90eb
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 03:15:23 -0300

    get info pom

--- vars/packageArtifact.groovy
+++ vars/packageArtifact.groovy
@@ -1,4 +1,6 @@
 def call() {
+    echo readMavenPom().getArtifactId()
+    echo readMavenPom().getVersion()
     echo "Realizando construção do artefato"
     sh "mvn -Dmaven.test.skip=true -Dmaven.test.failure.ignore clean package"
 }
