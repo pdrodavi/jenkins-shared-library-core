3173002e9fd74e0b7d987dd8c0e4fa95273d2235
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 09:16:07 -0300

    create repository private

--- vars/createRepositorySpringGitlab.groovy
+++ vars/createRepositorySpringGitlab.groovy
@@ -2,10 +2,12 @@ def call() {
     node {
 
         stage("Checkout") {
+            agent none
             gitCheckoutGitlab()
         }
 
         stage("Create Project") {
+            agent none
             createRepositoryGitlab()
         }
 
