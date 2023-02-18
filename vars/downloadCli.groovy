a3d6807736ecbf80d6c6815dcbf18e06854cf09c
Pedro Davi <pedrodaviti@gmail.com>
2023-02-18 05:31:10 -0300

    get job config

--- vars/downloadCli.groovy
+++ vars/downloadCli.groovy
@@ -4,11 +4,9 @@ def call() {
     sh "curl -# -O http://host.docker.internal:8080/jnlpJars/jenkins-cli.jar"
     sh "pwd"
     sh "ls -la"
-//    sh "java -jar jenkins-cli.jar -s http://host.docker.internal:8080/ -auth devops:11820e9b79637e6ae7cf649428d97b4352 -webSocket help"
 
     withCredentials([string(credentialsId: 'cli-jenkins-token', variable: 'JENKINSCLITOKEN')]) {
         sh "java -jar jenkins-cli.jar -s http://host.docker.internal:8080/ -auth devops:${JENKINSCLITOKEN} -webSocket help"
-//        sh "java -jar jenkins-cli.jar -s http://host.docker.internal:8080/ -auth devops:${JENKINSCLITOKEN} -webSocket create-job app-test"
     }
 
 }
--- vars/startContainer.groovy
+++ vars/startContainer.groovy
@@ -2,5 +2,5 @@ def call() {
     sh "docker rm -f ${readMavenPom().getArtifactId()}"
     sh "docker container create --name=${readMavenPom().getArtifactId()} --memory=256MB --memory-reservation=256MB --network=bridge --env-file=${env.WORKSPACE}/.env --restart=always --publish-all ${readMavenPom().getArtifactId()}:${readMavenPom().getVersion()}"
     sh "docker container start ${readMavenPom().getArtifactId()}"
-    cleanWs()
+//    cleanWs()
 }
